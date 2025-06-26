057490a1222efb9604784226ecfd2cf95e2a4b10
yerim Jeong <dnaer456@gmail.com>
2025-06-26 13:02:05 +0900

    아이콘 js 없애고 유저 페이지 js 생성

---
+++ instagram/js/user_page.js
@@ -0,0 +1,7 @@
+
+function show_Posting(){
+    document.querySelector(".posting").style.borderTop="solid 1px black";
+    document.querySelector(".stored_post").style.borderTop="none";
+    document.querySelector(".tag_post").style.borderTop="none";
+}
+
