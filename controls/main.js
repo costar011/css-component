0391f714acee3d9efe2e7e0dff0041c125802872
yerim Jeong <dnaer456@gmail.com>
2025-07-08 23:27:24 +0900

    main js upload

---
+++ controls/main.js
@@ -0,0 +1,5 @@
+const usernames = ["costar", "costar1", "costar2"];
+
+const input = document.querySelector("#input"),
+  spinner = document.querySelector(".spinner"),
+  button = document.querySelector("button");
