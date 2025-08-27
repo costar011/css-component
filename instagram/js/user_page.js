60a480d0d2c35bd3a1e04a7841c1f099fad44d12
yerim Jeong <dnaer456@gmail.com>
2025-08-27 20:55:08 +0900

    js 색상 변경

--- instagram/js/user_page.js
+++ instagram/js/user_page.js
@@ -1,7 +1,27 @@
-
-function show_Posting(){
+function show_Posting() {
     document.querySelector(".posting").style.borderTop="solid 1px black";
     document.querySelector(".stored_post").style.borderTop="none";
     document.querySelector(".tag_post").style.borderTop="none";
+
+    document.querySelector(".posting").style.color="#000000";
+    document.querySelector(".stored_post").style.color="#737373";
+    document.querySelector(".tag_post").style.color="#737373";
+
+    document.querySelector(".posting_page").style.display="flex";
+    document.querySelector(".stored_post_page").style.display="none";
+    document.querySelector(".tag_post_page").style.display="none";
 }
 
+function show_Stored() {
+    document.querySelector(".posting").style.borderTop="none";
+    document.querySelector(".stored_post").style.borderTop="solid 1px black";
+    document.querySelector(".tag_post").style.borderTop="none";
+
+    document.querySelector(".posting").style.color="#737373";
+    document.querySelector(".stored_post").style.color="#000000";
+    document.querySelector(".tag_post").style.color="#737373";
+    
+    document.querySelector(".posting_page").style.display="none";
+    document.querySelector(".stored_post_page").style.display="flex";
+    document.querySelector(".tag_post_page").style.display="none";
+}
